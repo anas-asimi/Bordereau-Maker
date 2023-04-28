efeacbc906bce07b06f9aebda76e1c006a9974c7
Anas Asimi <anas2020cr7@gmail.com>
2023-04-28 20:26:18 +0100

    add view engine complete post handler add 2 route for testing

--- server/index.js
+++ server/index.js
@@ -9,21 +9,40 @@ const app = express()
 app.use(express.static('static'))
 app.use(express.json())
 app.use(requestLogger)
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
 app.post('', async (request, response) => {
 
     let data = request.body
-    if (isValid(data)) return response.status(200).send()
+    if (isValid(data)) {
+        response.contentType("application/pdf");
+        let pdf = await pageGenerator(data)
+        return response.send(pdf);    
+    }
     return response.status(400).send()
 })
 
+
+let data = {
+    number: '305',
+    date: '2023-04-28',
+    sender: 'LE SERVICE DE MISE EN ŒUVRE DES PROJETS',
+    receiver: 'LE SERVICE DES AIDES ET INCITATIONS',
+    content: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin',
+    quantite: '1'
+}
+
+// this test endpoint return the generated page as html
 app.get('/test', async (request, response) => {
-    console.time();
-    let pdf = await pageGenerator()
-    // response.contentType("application/pdf");
-    console.timeEnd()
-    response.send();
+    response.render('borderaux',data)
+})
+
+
+// this test endpoint return the generated page as pdf
+app.get('/pdf-test', async (request, response) => {
+    let pdf = await pageGenerator(data)
+    response.contentType("application/pdf");
+    response.send(pdf);
 
 })
 
--- server/utils/pageGenerator.js
+++ server/utils/pageGenerator.js
@@ -14,7 +14,7 @@ export async function pageGenerator(data) {
 
     const FILE_PATH = `${process.cwd()}\\views\\borderaux.ejs`
     const page = await browser.newPage();
-    const html = await ejs.renderFile(FILE_PATH, { data });
+    const html = await ejs.renderFile(FILE_PATH, data);
     await page.setContent(html);
     const pdf = await page.pdf({ format: "A4" });
     await page.close()
