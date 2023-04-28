6e67e69d5043b2307c3eb3ff74b37a353edbe73f
Anas Asimi <anas2020cr7@gmail.com>
2023-04-28 20:12:06 +0100

    add ability to download file from axious response

--- client/main.js
+++ client/main.js
@@ -30,5 +30,21 @@ function generate() {
     }
 
     console.log(data);
-    axios.post('/', data).catch((err) => console.log(err))
+    axios.post('/', data,{ responseType: 'blob' })
+        .then((response) => {
+            // create file link in browser's memory
+            const href = URL.createObjectURL(response.data);
+
+            // create "a" HTML element with href to file & click
+            const link = document.createElement('a');
+            link.href = href;
+            link.setAttribute('download', 'file.pdf'); //or any other extension
+            document.body.appendChild(link);
+            link.click();
+
+            // clean up "a" element & remove ObjectURL
+            document.body.removeChild(link);
+            URL.revokeObjectURL(href);
+        })
+        .catch((err) => console.log(err))
 }
