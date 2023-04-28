4115dd6164a8b127bb2fbccfa5bc91ae2a9498fe
Anas Asimi <anas2020cr7@gmail.com>
2023-04-28 11:45:27 +0100

    log date in requestLogger

--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -1,6 +1,7 @@
 import logger from './logger.js'
 
 const requestLogger = (request, response, next) => {
+  logger.info('Date :', (new Date()).toISOString())
   logger.info('Method :', request.method)
   logger.info('Path :  ', request.path)
   logger.info('Body :  ', request.body)
