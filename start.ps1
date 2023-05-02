40125072c38084587242397b227946c399751eed
Anas Asimi <anas2020cr7@gmail.com>
2023-05-02 14:53:40 +0100

    make start script remove old static files before build

--- start.ps1
+++ start.ps1
@@ -1,1 +1,5 @@
-npm run build --prefix client/ ; npm run dev --prefix server/
+Remove-Item ./server/static/* -Recurse -Force
+# build static files
+npm run build --prefix client/
+# run server
+npm run dev --prefix server/
